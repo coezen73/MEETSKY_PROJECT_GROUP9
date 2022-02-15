950b78275c82b0d0bde95f8fec98cbdbe16df095
coezen73 <85904903+coezen73@users.noreply.github.com>
2022-02-15 23:52:08 +0100

    login StepDefs is created

--- src/test/java/net/meetsky/runners/SkyRunner.java
+++ src/test/java/net/meetsky/runners/SkyRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "net/meetsky/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@Cemil"
 )
 public class SkyRunner {
 }
