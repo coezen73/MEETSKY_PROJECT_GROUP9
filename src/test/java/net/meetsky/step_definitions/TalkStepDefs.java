48cc6d21fe030a58ad8c611c6c8432164ad8a241
ibrahim ucar <90156821+ibrhmucar@users.noreply.github.com>
2022-02-23 20:03:59 +0300

    Update TalkStepDefs.java

--- src/test/java/net/meetsky/step_definitions/TalkStepDefs.java
+++ src/test/java/net/meetsky/step_definitions/TalkStepDefs.java
@@ -33,8 +33,8 @@ public class TalkStepDefs {
 
     @Given("the user logged in")
     public void the_user_logged_in() {
-        SkyDriver.get().get(ConfigurationReader.get("url"));
-        loginPage.login(ConfigurationReader.get("username"), ConfigurationReader.get("password"));
+       SkyDriver.get().get(ConfigurationReader.get("url"));
+       loginPage.login(ConfigurationReader.get("username"),ConfigurationReader.get("password"));
 
 
     }
@@ -83,55 +83,55 @@ public class TalkStepDefs {
         }
 
     }
-        @When("Click {string} inbox")
-        public void click_inbox(String string){
-            new MainPage().talk.click();
-            SkyUtils.waitForPageToLoad(5);
-        }
+    @When("Click {string} inbox")
+    public void click_inbox(String string) {
+        new MainPage().talk.click();
+        SkyUtils.waitForPageToLoad(5);
+    }
 
-        @When("Enter {string} into Search conversations or users inbox")
-        public void enter_into_inbox (String name){
-            TalkModulePage talkPage = new TalkModulePage();
-            talkPage.searchInputBox.sendKeys(name + Keys.ENTER);
+    @When("Enter {string} into Search conversations or users inbox")
+    public void enter_into_inbox(String name) {
+        TalkModulePage talkPage = new TalkModulePage();
+        talkPage.searchInputBox.sendKeys(name+ Keys.ENTER);
 
-        }
+    }
 
-        @When("Select user {string}")
-        public void select_user(String contactName){
-            WebElement contactInList = SkyDriver.get().findElement(By.xpath("//span[contains(.,'" + contactName + "')]"));
-            contactInList.click();
-        }
+    @When("Select user {string}")
+    public void select_user(String contactName) {
+        WebElement contactInList = SkyDriver.get().findElement(By.xpath("//span[contains(.,'"+contactName+"')]"));
+        contactInList.click();
+    }
 
-        @When("Click on Start call button")
-        public void click_on_button() throws InterruptedException {
-            TalkModulePage talkPage = new TalkModulePage();
-            SkyUtils.waitForClickablility(talkPage.startCallButton, 5);
+    @When("Click on Start call button")
+    public void click_on_button() throws InterruptedException {
+        TalkModulePage talkPage = new TalkModulePage();
+        SkyUtils.waitForClickablility(talkPage.startCallButton,5);
 //        JavascriptExecutor jse = (JavascriptExecutor) SkyDriver.get();
 //        jse.executeScript("arguments[0].click();",talkPage.startCallButton);
-            SkyDriver.get().navigate().refresh();
-            try {
-                talkPage.startCallButton.click();
-            } catch (Exception e) {
-                System.out.println("Did not click on Start call button");
-            }
-
-            try {
-                Alert alert = SkyDriver.get().switchTo().alert();
-                alert.accept();
-            } catch (Exception e) {
-                System.out.println("Mic&video message is not appeared");
-
-            }
-
+        SkyDriver.get().navigate().refresh();
+        try {
+            talkPage.startCallButton.click();
+        } catch (Exception e) {
+            System.out.println("Did not click on Start call button");
         }
 
-        @Then("Verify video call page is shown.")
-        public void verify_video_call_page_is_shown(){
-            TalkModulePage talkPage = new TalkModulePage();
-            SkyUtils.waitForVisibility(talkPage.waitingMessage, 5);
-            Assert.assertTrue(talkPage.waitingMessage.isDisplayed());
-            SkyUtils.waitFor(5);
+        try {
+            Alert alert= SkyDriver.get().switchTo().alert();
+            alert.accept();
+        } catch (Exception e) {
+            System.out.println("Mic&video message is not appeared");;
         }
+
+    }
+
+    @Then("Verify video call page is shown.")
+    public void verify_video_call_page_is_shown() {
+        TalkModulePage talkPage = new TalkModulePage();
+        SkyUtils.waitForVisibility(talkPage.waitingMessage,5);
+        Assert.assertTrue(talkPage.waitingMessage.isDisplayed());
+        SkyUtils.waitFor(5);
+    }
+
     @When("Click on {string} option on the right sidebar.")
     public void click_on_option_on_the_right_sidebar(String string) {
         TalkModulePage talkPage = new TalkModulePage();
@@ -146,6 +146,7 @@ public class TalkStepDefs {
         List<String> participants = SkyUtils.getElementsText(talkPage.participantList);
         Assert.assertTrue(participants.contains(userName));
         Assert.assertTrue(participants.contains(participantName));
+
 
     }
 }
