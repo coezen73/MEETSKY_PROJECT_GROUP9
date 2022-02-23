fd6d601bcb4db0a1bd7b71976299deb86232228a
Bilgin <bilginkalmis@gmail.com>
2022-02-23 17:54:53 +0100

    Add functionality is done.

--- src/test/java/net/meetsky/runners/SkyRunner.java
+++ src/test/java/net/meetsky/runners/SkyRunner.java
@@ -7,17 +7,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty",
-                "json:target/cucumber.json",
+        plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "net/meetsky/step_definitions",
-        strict = true,
         dryRun = false,
-
-        tags = "@ETSKY-338"
-
-
+        tags = "@ETSKY-337"
+)
 public class SkyRunner {
 }
--- src/test/java/net/meetsky/step_definitions/FilesStepDefs.java
+++ src/test/java/net/meetsky/step_definitions/FilesStepDefs.java
@@ -1,4 +1,58 @@
 package net.meetsky.step_definitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.meetsky.pages.FilesPage;
+import net.meetsky.pages.TalkModulePage;
+import net.meetsky.utilities.SkyDriver;
+import net.meetsky.utilities.SkyUtils;
+import org.junit.Assert;
+
+
+import java.util.List;
+
 public class FilesStepDefs {
+    FilesPage filesPage = new FilesPage();
+    @When("user should be navigate the add icon module")
+    public void user_should_be_navigate_the_add_icon_module() {
+        SkyUtils.waitFor(1);
+        filesPage.addIcon.click();
+        SkyUtils.waitFor(3);
+    }
+    @Then("navigate to {string} button")
+    public void navigate_to_button(String buttonName) {
+        System.out.println("buttonName = " + buttonName);
+        filesPage.navigate(buttonName);
+
+    }
+
+    @When("user should be able to click {string} button")
+    public void user_should_be_able_to_click_button(String buttonName) {
+        filesPage.newFolderButton.click();
+    }
+
+    @Then("user should be able to write {string} file name")
+    public void user_should_be_able_to_write_file_name(String filename) {
+        filesPage.newFolder.clear();
+        filesPage.newFolder.sendKeys(filename);
+        SkyUtils.waitFor(2);
+    }
+
+    @Then("user should be able to click confirm icon")
+    public void user_should_be_able_to_click_confirm_icon() {
+        filesPage.confirm.click();
+        SkyUtils.waitFor(2);
+        SkyDriver.get().navigate().refresh();
+    }
+
+        @Then("verify new {string} file is shown on the page")
+    public void verify_new_file_is_shown_on_the_page(String filename) {
+
+        List<String> file = SkyUtils.getElementsText(filesPage.check);
+        SkyUtils.waitFor(3);
+        Assert.assertTrue(file.containsAll(file));
+
+
+    }
+
 }
